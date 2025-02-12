8f3aece2f053f29e4d8da76eab159a43a7ec74e7
RJ Sheperd <rjsheperd@gmail.com>
2025-02-12 13:27:07 -0800

    Add sign.sh

---
+++ sign.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Inputs
+JAR=$1
+
+jarsigner -J-Djava.class.path=/app/lib/Digicert/digicert-jce-1.0.jar:/app/lib/Digicert/bcprov-jdk18on-1.77.jar \
+  -keystore NONE \
+  -storetype DIGICERT \
+  -storepass changeit \
+  -providerClass com.digicert.jce.Provider \
+  -signedjar "${JAR%.*}-signed.jar"\
+  -sigalg SHA256withRSA \
+  -tsa "http://timestamp.digicert.com" \
+  "$JAR" \
+  "$SM_KEYPAIR" \
+  --verbose \
+  --debug
